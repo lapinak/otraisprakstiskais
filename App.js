5ac708e850e4b219271ee8359e7f85672542e5cb
KarinaAnna <48749638+lapinak@users.noreply.github.com>
2020-12-09 22:02:22 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,38 +1,29 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
-
-export default function App() {
+const HelloWorld = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
+     <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+    <Text style={styles.baseText}>
+      Hello World from Group 1 : Roberts Bišs!
+      <Text style={styles.innerText}>This is my Roberts Bišs first React Native application!</Text>
+    </Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+  baseText: {
+    fontWeight: 'bold'
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+  innerText: {
+    color: 'red'
+  }
 });
+
+export default HelloWorld;
